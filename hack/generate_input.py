9bcdda56cde36b8b306dfa93a1a050420910f160
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-09-16 13:59:13 +0200

    generate_input: cleverer generate_optimal_solution()

--- hack/generate_input.py
+++ hack/generate_input.py
@@ -3,7 +3,7 @@
 
 import sys, random
 
-def size_of_solution(solution):
+def weight_of_solution(solution):
     size = 0
     for item in solution:
         size += item[0]
@@ -27,14 +27,21 @@ def print_optimal(opt):
 
 
 
-def generate_optimal_solution(num_items, capacity, min_value, max_value):
+def generate_optimal_solution(capacity):
     opt = []
-    for i in range(0, num_items):
-        w = capacity / num_items # size
-        v = random.randint(min_value, max_value) # value
+    remaining_capacity = capacity
+    min_size, max_size = 2, 12
+    vpw_factor = 5 # value per weight factor.
+
+    while remaining_capacity > 0:
+        # size
+        w = random.randint(min(min_size, remaining_capacity), min(max_size, remaining_capacity))
+        remaining_capacity -= w
+        # value
+        v = random.randint(w*vpw_factor, (w*2)*vpw_factor)
         opt.append((w,v))
 #    print >> sys.stderr, "generated optimal solution of %d items with total size %d and value %d" \
-#            % (num_items, size_of_solution(opt), value_of_solution(opt))
+#            % (len(opt), weight_of_solution(opt), value_of_solution(opt))
     return opt
 
 def generate_random_items(num_items, max_value_per_weight_ratio):
@@ -45,8 +52,8 @@ def generate_random_items(num_items, max_value_per_weight_ratio):
         max_w = int(max_value_per_weight_ratio/v)
         w = random.randint(min(1,max_w), max_w)
         items.append((w,v))
-#    print >> sys.stderr, "generated %d random items with total size %d and value %d" \
-#            % (num_items, size_of_solution(items), value_of_solution(items))
+    print >> sys.stderr, "generated %d random items with total size %d and value %d" \
+            % (num_items, weight_of_solution(items), value_of_solution(items))
     return items
 
 
@@ -56,25 +63,19 @@ def generate_random_items(num_items, max_value_per_weight_ratio):
 # capacity - capacity of knapsack
 # n - the number of items to generate.
 # m - the number of items in generated optimal solution.
-def generate_input(capacity=32, n=1000, m=8):
+def generate_input(capacity=32, n=1000):
 
     # strategy:
     # define capacity of knapsack,
     # distribute capacity over m items and give those items a very high weight.
     # make sure those items are optimal by not giving any other item a higher weight.
 
-    opt_min_weight, opt_max_weight = 10, 20
-    
     # generate the optimal solution.
-    # the optimal solution has value >= m*opt_min_weight
-    # and value-per-weight >= (m*opt_min_weight) / capacity
-    opt = generate_optimal_solution(m, capacity, opt_min_weight, opt_max_weight)
+    opt = generate_optimal_solution(capacity)
     
-    min_opt_value = (m*opt_min_weight) / float(capacity)
-    opt_value_per_weight = value_of_solution(opt) / float(size_of_solution(opt))
-    
-    # generate n-m items that can not be part of the optimal solution.
-    items = generate_random_items(n-m, opt_value_per_weight)
+    # generate items that can not be part of the optimal solution.
+    opt_value_per_weight = value_of_solution(opt) / float(weight_of_solution(opt))
+    items = generate_random_items(n-len(opt), opt_value_per_weight)
     items += opt
     random.shuffle(items)
     
